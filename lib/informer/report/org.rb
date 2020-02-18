1707ef462d7254f778340b9d6e3f24bb94718daa
andersonreinkordt <anderson.reinkordt@paymentspring.com>
2020-02-17 19:57:51 -0600

    Remove comments

--- lib/informer/report/org.rb
+++ lib/informer/report/org.rb
@@ -17,40 +17,6 @@ module Informer
         #{result[:contents].join("\n")}
         TEXT
       end.join("\n")
-
-      # @results.split("\n").map do |result|
-
-      #   <<~TEXT
-      #   * TITLE GOES HERE
-      #     #{result}
-      #   ** CONTENT GOES HERE
-      #   TEXT
-      # end.join("\n")
-
-      # @results.lines.map do |result|
-      #   begin
-      #     # TODO: add filtering/validation
-      #     next if result == "\n"
-
-      #     result = result.force_encoding('UTF-8')
-
-      #     # byebug unless result.match?(/\//)
-      #     # byebug unless result.match?(/^\d/)
-
-      #     byebug
-
-      #     title = result.first
-      #     contents = result[1..-1].to_s
-
-      #     <<~TEXT
-      #            * #{title}
-      #            ** #{contents}
-      #     TEXT
-      #   rescue => e
-      #     puts e.inspect
-      #     byebug
-      #   end
-      # end.join("\n")
     end
   end
 end
